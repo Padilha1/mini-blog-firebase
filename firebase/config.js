0138c3a7fb15019345ecfdfdf3fc0ee7097de56e
Padilha1 <padilha.matheus@hotmail.com>
2022-10-30 15:25:18 -0300

    First Commit

---
+++ firebase/config.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import {getFirestore} from "firebase/firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAPmae_IPQYA-B6IHfIuxSTXx2d6pmOHlw",
+  authDomain: "miniblog-df8c6.firebaseapp.com",
+  projectId: "miniblog-df8c6",
+  storageBucket: "miniblog-df8c6.appspot.com",
+  messagingSenderId: "762622577878",
+  appId: "1:762622577878:web:3e3e0c6de48b8ef24f4d77"
+};
+
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app)
---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './css/app.css'
+
+function App() {
+  
+  return (
+    <div className="App">
+      Hello World
+    </div>
+  )
+}
+
+export default App
